08544d035e05260532d9e8731954d71135ef5e51
buptsky <18801186798@163.com>
2017-09-29 16:46:02 +0800

    增加panel可选项

--- src/components/common/panel/panel.js
+++ src/components/common/panel/panel.js
@@ -4,7 +4,7 @@
  * 面板本身不具备关闭开启功能，该功能由父组件控制
  * props参数（可选）
  * panelTitle[string]面板类型/ cancel[Function]面板取消回调/
- * confirm[Function]面板确认回调/ showBtn[Boolean]是否展示操作按钮/
+ * confirm[Function]面板确认回调/ disableBtn[Boolean]是否展示操作按钮/
  */
 import React from 'react';
 import './panel.css';
