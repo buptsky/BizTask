0e6916969e7f41641fa7f694e4a2552dad086878
zhaiyb <zhaiyibo10@gmail.com>
2017-09-30 10:47:02 +0800

    fetchData，css to less

--- src/components/task/ChargeTask.js
+++ src/components/task/ChargeTask.js
@@ -1,36 +1,67 @@
-import {Layout} from 'antd';
-const {Sider, Content} = Layout;
+import {actionCreator, actionTypes} from '../../action-creator';
+import {Layout, Spin} from 'antd';
 import {connect} from 'react-redux';
-import {actionCreator, actionTypes} from '../../actions/action-creator';
 import TaskMenu from './TaskMenu';
+import TaskList from './TaskList';
+
+const {Sider, Content} = Layout;
+
 @connect(
-    state => ({}),
-    dispatch => ({
-        activeHeaderMenu: ()=> {
-            dispatch(actionCreator('change_header_menu', 'task'));
-        }
-    })
+  state => ({}),
+  dispatch => ({
+    activeHeaderMenu: () => {
+      dispatch(actionCreator('change_header_menu', 'task'));
+    }
+  })
 )
 class Task extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      dataSource: []
+    };
+  }
 
-    componentDidMount() {
-        this.props.activeHeaderMenu();
-    }
+  componentDidMount() {
+    this.props.activeHeaderMenu();
+    this.getTasks();
+  }
 
-    render() {
-        return (
-            <Layout>
-                <Sider>
-                    <TaskMenu activeKey="chargeTask"/>
-                </Sider>
-                <Content>
-                    <h1>chargeTask</h1>
-                </Content>
-            </Layout>
-        );
-    }
+  getTasks = () => {
+    const queryArgs = {
+      userType: 0,  //我负责的任务0，我关注的任务1
+      viewType: 0   //标签视图，将要废弃
+    };
+    fetchData({
+      url: '/task/getTasks.do',
+      data: queryArgs
+    }).then((data) => {
+      this.setState({
+        loading: false,
+        dataSource: data
+      });
+    });
+  };
+  renderTasks = (dataSource) => {
+    return dataSource.map((taskList) => {
+      return <TaskList key={taskList.name} name={taskList.name} dataSource={taskList.taskList}/>
+    });
+  };
+
+  render() {
+    const {loading, dataSource} = this.state;
+    return (
+      <Layout>
+        <Sider>
+          <TaskMenu activeKey="chargeTask"/>
+        </Sider>
+        <Content style={{padding: "20px"}}>
+          {loading ? <Spin tip="Loading..."/> : this.renderTasks(dataSource)}
+        </Content>
+      </Layout>
+    );
+  }
 }
+
 export default Task;
--- theme.js
+++ theme.js
@@ -1,5 +1,7 @@
 const theme = {
-    "@font-size-base": "14px",
-    "@text-color": "#333"
+  "@primary-color": "#108ee9",
+  "@primary-text-color": "#fff",
+  "@font-size-base": "14px",
+  "@text-color": "#333"
 };
 module.exports = theme;
